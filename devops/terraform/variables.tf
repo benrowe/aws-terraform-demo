fdfccaca6e999c87f2f8238a0e1f0a267601b1b7
Ben Rowe <ben.rowe.83@gmail.com>
2020-10-06 11:35:17 +1100

    Add basic terraform setup

---
+++ devops/terraform/variables.tf
@@ -0,0 +1,9 @@
+// Provider details
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    type    = string
+    default = "ap-southeast-2"
+}
+
+variable "stack_name" {}
