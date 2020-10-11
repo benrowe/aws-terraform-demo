d0450adfddf25b8f66df775c3cbac13558f4e0c6
Ben Rowe <ben.rowe.83@gmail.com>
2020-10-12 08:08:36 +1100

    Add support for redis queues

--- devops/docker/worker/command-dev.sh
+++ devops/docker/worker/command-dev.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-composer install
+set -e
 
-./artisan queue:work
+role=${CONTAINER_ROLE:-worker}
+
+echo "Starting $role"
+
+if [ "$role" = "worker" ]; then
+    echo "Running the worker"
+    php artisan queue:work --verbose --tries=3 --timeout=90
+elif [ "$role" = "scheduler" ]; then
+    echo "Running the scheduler"
+    while [ true ]
+    do
+      php artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
+fi
+
+
