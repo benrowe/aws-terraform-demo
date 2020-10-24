5f9b2b77d79cc937325d8ced9112b4eb4c8ff1c2
Ben Rowe <ben.rowe.83@gmail.com>
2020-10-24 13:20:25 +1100

    Provide output for ecr

--- devops/terraform/output.tf
+++ devops/terraform/output.tf
@@ -2,3 +2,14 @@ output "api_host" {
   value = ""
 }
 
+output "web_uri" {
+  value = module.ecr_web.url
+}
+
+output "app_uri" {
+  value = module.ecr_app.url
+}
+
+output "worker_uri" {
+  value = module.ecr_worker.url
+}
