0a9605c9fa8f36163768e323fab31502df4436f2
Ben Rowe <ben.rowe.83@gmail.com>
2020-10-24 13:20:10 +1100

    Move backend to s3

--- devops/terraform/backend.tf
+++ devops/terraform/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   required_version = ">= 0.13"
-  //  backend "s3" {
-  //    bucket = "benrowe-deploys"
-  //    key = "terraform/states/laravel-aws-terraform/terraform.tfstate"
-  //    region = "ap-southeast-2"
-  //  }
+    backend "s3" {
+      bucket = "benrowe-deploys"
+      key = "terraform/states/laravel-aws-terraform/terraform.tfstate"
+      region = "ap-southeast-2"
+      profile = "aws-terraform-demo"
+    }
   required_providers {
     aws = {
       source = "hashicorp/aws",
