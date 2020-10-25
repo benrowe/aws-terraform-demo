9552f4a911178bacd7076484d2100d7d95451fd0
Ben Rowe <ben.rowe.83@gmail.com>
2020-10-26 07:26:20 +1100

    Pass through vpn details to service

--- devops/terraform/modules/service-application/main.tf
+++ devops/terraform/modules/service-application/main.tf
@@ -1,34 +1,64 @@
+data "aws_iam_role" "task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
+data "template_file" "hello_world" {
+  template = "${file("${path.module}/tasks/hello-world.json")}"
+  vars = {
+
+  }
+}
+
 resource "aws_ecs_task_definition" "hello_world" {
   family = "hello_world"
   requires_compatibilities = [
-    "fargate"
+    "FARGATE"
   ]
 
-  container_definitions = <<EOF
-[
-  {
-    "name": "hello_world",
-    "image": "hello-world",
-    "cpu": 0,
-    "memory": 128,
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "options": {
-        "awslogs-region": "ap-southeast-2",
-        "awslogs-group": "hello_world",
-        "awslogs-stream-prefix": "complete-ecs"
-      }
-    }
+  execution_role_arn = data.aws_iam_role.task_execution_role.arn
+
+  cpu          = "256"
+  memory       = "512"
+  network_mode = "awsvpc"
+
+  container_definitions = data.template_file.hello_world.rendered
+}
+
+resource "aws_security_group" "hello_world_task" {
+  name        = "${var.name}-esc-hello-world-task"
+  description = "Allow inbound access"
+  vpc_id      = var.vpc
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
   }
-]
-EOF
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = var.tags
 }
 
 resource "aws_ecs_service" "hello_world" {
-  name                               = "hello_world"
-  cluster                            = var.cluster_id
-  task_definition                    = aws_ecs_task_definition.hello_world.arn
+  name            = "hello_world"
+  cluster         = var.cluster_id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+
+  launch_type = "FARGATE"
+
   desired_count                      = 1
   deployment_maximum_percent         = 100
   deployment_minimum_healthy_percent = 0
+  network_configuration {
+    subnets          = []
+    security_groups  = [aws_security_group.hello_world_task.id]
+    assign_public_ip = true
+
+  }
+  tags = var.tags
 }
--- devops/terraform/modules/service-application/variables.tf
+++ devops/terraform/modules/service-application/variables.tf
@@ -1,4 +1,19 @@
+variable "name" {
+  description = "The name of the repository"
+}
+
 variable "cluster_id" {
   description = "The ECS cluster ID"
   type        = string
+}
+
+variable "vpc" {
+  description = "The vpc put this service into"
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources"
+  type        = map(string)
+  default     = {}
 }
